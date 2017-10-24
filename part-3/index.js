42e227ea6aef76885a62463036f353d68bd4df96
Jonathan Lubaway <NeoJax@gmail.com>
2017-10-24 00:49:05 -0700

    Managed to get clicking on a button working without jQuery

---
+++ part-3/index.js
@@ -0,0 +1,6 @@
+document.getElementsByName('button').onclick = function() {console.log('lol');};
+
+function showModal() {
+  console.log('lol');
+  // document.getElementById('bookModalBG').style.display = 'true';
+}
