e56d0b1930a68cab96582dce86a4286eecf6c313
Jonathan Lubaway <NeoJax@gmail.com>
2017-10-24 18:13:23 -0700

    Finished part-3, working on tests for part-2

--- part-3/index.js
+++ part-3/index.js
@@ -1,6 +1,30 @@
-document.getElementsByName('button').onclick = function() {console.log('lol');};
-
 function showModal() {
-  console.log('lol');
-  // document.getElementById('bookModalBG').style.display = 'true';
+  document.getElementById('bookModalBG').style.display = '';
+  const cost = this.parentNode.previousSibling.previousSibling;
+  const capacity = cost.previousSibling.previousSibling;
+  const room = capacity.previousSibling.previousSibling;
+  document.getElementById('cost').innerHTML = cost.innerHTML;
+  document.getElementById('room').innerHTML = room.innerHTML;
+}
+
+function updateTotal() {
+  const nights = this.value;
+  let cost = this.parentNode.childNodes[5].childNodes[0].innerHTML;
+  cost = cost.slice(1);
+  cost = parseFloat(cost);
+  const total = cost * nights;
+  document.getElementById('total').innerHTML = `$${total}`;
+}
+
+function hideModal() {
+  document.getElementById('bookModalBG').style.display = 'none';
+}
+
+window.onload = function () {
+  const buttons = document.getElementsByClassName("bookBtn");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showModal);
+  }
+  const numbers = document.getElementById('nights')
+  numbers.addEventListener('change', updateTotal);
 }
